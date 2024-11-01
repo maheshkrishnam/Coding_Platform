08f4e4b2944044e226f78c80acc551a13af2cdab
Mahesh Krishnam <131548711+maheshkrishnam@users.noreply.github.com>
2024-11-01 15:28:22 +0530

    Basic setup is done

---
+++ server/src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// use :: use is generally used for middlewares
+// for cross origin sharing for all origin belonging to CORS_ORIGIN 
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
+
+// to parse the json and make it available in req.body and setting limit on the recieving payload
+app.use(express.json({
+  limit: "16kb",
+}));
+
+// encode the url
+app.use(express.urlencoded({
+  extended: true,
+  limit: "16kb"
+}));
+
+// to store local files in public directory
+app.use(express.static("public"));
+
+
+export default app;
